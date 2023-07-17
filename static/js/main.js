2f23618fb99b2b5944efb5d71414c78678f45e2b
faberbg <faber@Davids-MacBook-Pro.local>
2023-07-18 01:23:29 +0200

    Added templates for my chat button, 3 different type of pop screens, and js code to hide and show needed element

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,40 @@
+/**
+ * Variables
+ */
+let chatName = ''
+let chatScoket = null
+let chatWindowUrl = window.location.href
+let chatRoomUuid = Math.random().toString(36).slice(2,12)
 
+/**
+ * ELEMENTS
+ */
+
+const chatElement = document.querySelector('#chat')
+const chatOpenElement = document.querySelector('#chat_open')
+const chatJoinElement = document.querySelector('#chat_join')
+const chatIconElement = document.querySelector('#chat_open')
+const chatWelcomeElement = document.querySelector('#chat_welcome')
+const chatRoomElement = document.querySelector('#chat_room')
+
+/**
+ * Event listeners
+ */
+
+chatOpenElement.onclick = function(e){
+    e.preventDefault();
+    
+    chatIconElement.classList.add('hidden')
+    chatWelcomeElement.classList.remove('hidden')
+
+    return false
+}
+
+chatJoinElement.onclick = function(e){
+    e.preventDefault();
+    
+    chatWelcomeElement.classList.add('hidden')
+    chatRoomElement.classList.remove('hidden')
+
+    return false
+}
